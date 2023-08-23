911b8f1d28f49ded3f7c8bc3344de4d76675dc30
Drishtant Ranjan Srivastava <84273332+Drishtantranjan@users.noreply.github.com>
2023-08-23 08:11:52 +0530

    Update BruteForce.java

--- Daimler Trucks/String Transformation/BruteForce.java
+++ Daimler Trucks/String Transformation/BruteForce.java
@@ -43,3 +43,53 @@ public class transformation {
     }
 }
  
+```````````````````````````````````````````````````````
+import java.io.*;
+import java.util.*;
+ 
+public class transformation {
+
+    public static int minOps(String A, String B)
+    {
+        if (A.length() != B.length())
+            return -1;
+ 
+        int i, j, res = 0;
+        int count[] = new int[256];
+        for (i = 0; i < A.length(); i++) {
+            count[A.charAt(i)]++;
+            count[B.charAt(i)]--;
+        }
+
+        for (i = 0; i < 256; i++)
+            if (count[i] != 0)
+                return -1;
+ 
+        i = A.length() - 1;
+        j = B.length() - 1;
+ 
+        while (i >= 0) {
+            if (A.charAt(i) != B.charAt(j))
+                res++;
+            else
+                j--;
+            i--;
+        }
+        return res;
+    }
+ ////////////// Just for Example //////////////////////
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);   
+        System.out.print("Enter a string A: ");  
+        String A= sc.nextLine();   
+            
+        System.out.print("Enter a string B: ");  
+        String B= sc.nextLine();   
+ 
+        System.out.println(
+            "Minimum number of operations required is "
+            + minOps(A, B));
+    }
+}
+`````````````````````````````````````````````````````````````
